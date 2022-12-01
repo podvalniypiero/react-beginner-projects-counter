ebcebca62182f30d0d2aef19c0b35256f1046083
podvalniypiero <podvalniypiero@mail.ru>
2022-12-01 12:21:49 +0300

    quiz init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
-import React from 'react';
 import './index.scss';
 
-const Modal = ({open,setOpen}) => (
-  <div className={`overlay animated ${ open ? `show` : ``}`}>
-  <div className="modal">
-    <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
-      <title />
-      <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-    </svg>
-    <img src="https://media1.giphy.com/media/WUTywPPYZpdDChyBaZ/giphy.gif?cid=ecf05e47iz54bz5zsuos4pnme352rnyjbgfbewcgvjmpzcvf&rid=giphy.gif&ct=g" />
-  </div>
-</div>
+const questions = [
+  {
+    title: 'React - это ... ?',
+    variants: ['библиотека', 'фреймворк', 'приложение'],
+    correct: 0,
+  },
+  {
+    title: 'Компонент - это ... ',
+    variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
+    correct: 1,
+  },
+  {
+    title: 'Что такое JSX?',
+    variants: [
+      'Это простой HTML',
+      'Это функция',
+      'Это тот же HTML, но с возможностью выполнять JS-код',
+    ],
+    correct: 2,
+  },
+];
 
-);
+function Result() {
+  return (
+    <div className="result">
+      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
+      <h2>Вы отгадали 3 ответа из 10</h2>
+      <button>Попробовать снова</button>
+    </div>
+  );
+}
 
-  function App() {
-    const [open,setOpen] = React.useState(false);
-    return (
-      <div className="App">
-        <button onClick={() => setOpen(true)} className="open-modal-btn">✨Click here to open</button>
-      {open && <Modal open={open} setOpen={setOpen} />}
+function Game() {
+  return (
+    <>
+      <div className="progress">
+        <div style={{ width: '50%' }} className="progress__inner"></div>
       </div>
-    );
-  }
+      <h1>Что такое useState?</h1>
+      <ul>
+        <li>Это функция для хранения данных компонента</li>
+        <li>Это глобальный стейт</li>
+        <li>Это когда на ты никому не нужен</li>
+      </ul>
+    </>
+  );
+}
 
+function App() {
+  return (
+    <div className="App">
+      <Game />
+      {/* <Result /> */}
+    </div>
+  );
+}
 
 export default App;
