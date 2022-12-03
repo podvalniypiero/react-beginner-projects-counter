fd416cc02ed2ac7ae89a28ff9f5f56508f52925c
podvalniypiero <podvalniypiero@mail.ru>
2022-12-03 21:32:53 +0300

    initial currency

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React from 'react';
+import { Block } from './Block';
 import './index.scss';
 
 function App() {
-  const [count,setCount] = React.useState(0);
-  const onClickPlus = () => {
-    setCount(count + 1);
-  }
-  const onClickMinus = () => {
-    setCount(count - 1);
-  }
   return (
     <div className="App">
-      <div>
-        <h2>Let's count:</h2>
-        <h1>{count}</h1>
-        <button onClick={onClickMinus} className="minus">- minus</button>
-        <button onClick={onClickPlus} className="plus">plus +</button>
-      </div>
+      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
+      <Block value={0} currency="USD" />
     </div>
   );
 }
