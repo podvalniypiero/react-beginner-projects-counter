aee6be35f90527e29684326b34b2c777135202e0
podvalniypiero <podvalniypiero@mail.ru>
2022-12-04 15:21:39 +0300

    api fetch

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import { Block } from './Block';
 import './index.scss';
 
 function App() {
+  const [valute,setValute] = React.useState({});
+
+  React.useEffect(() => {
+    fetch('https://www.cbr-xml-daily.ru/daily_json.js')
+    .then(res => res.json())
+    .then((json)=> {
+      setValute(json.Valute);
+      console.log(json.Valute);
+
+    })
+    .catch(err => {
+      console.log(err);
+      alert('Error! No data from server...')
+    })
+  },[])
+
   return (
     <div className="App">
       <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
